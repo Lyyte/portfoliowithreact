c0937d73d6a73f0fe723a990dac9217587332edd
Lyyte <lyytefaridi@gmail.com>
2020-07-04 18:34:33 -0500

    added carousel, using card and card info comps, adding styling

--- portfoliosite/src/pages/HomePage.js
+++ portfoliosite/src/pages/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
 import Banner from '../components/Banner'
+import Carousel from '../components/Carousel'
 
 function HomePage(props) {
     return(
-        <Banner title={props.title} subTitle={props.subTitle} text={props.text} />
+        <div>
+            <Banner title={props.title} subTitle={props.subTitle} text={props.text} />
+            <Carousel />
+        </div>
     )
 }
 
